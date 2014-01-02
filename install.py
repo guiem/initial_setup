f720bffcb1034380c9fa86768dbc4881eb491c86
guiem <g@guiem.info>
2014-01-02 17:14:08 +0100

    initial commit

---
+++ install.py
@@ -0,0 +1,18 @@
+import pip
+from settings import *
+try:
+    import fabric
+except:
+    print 'Installing fabric...'
+    pip.main(['install'],'fabric')
+    print 'Fabric installed!'
+from fabric.api import *
+
+for server in SERVERS:
+    env.host_string = server[0]
+    env.user = server[1]
+    env.password = server[2]
+    env.port = server[3]
+    env.warn_only = True
+    sudo('apt-get install python-pip')
+
---
+++ settings.py
@@ -0,0 +1,7 @@
+SERVERS = [('your_remote_machine_address','user','password','port')]
+
+try:
+    from settings_local import *
+except:
+    pass
+
